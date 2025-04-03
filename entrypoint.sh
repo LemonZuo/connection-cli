764742c78841f3ef770b3ba4a6071e757a67aa2e
Lemon <lemon@linux.do>
2025-04-03 21:20:37 +0800

    refactor entrypoint.sh to suppress version output; improve log initialization messages for clarity

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,13 @@ if [ ! -f /app/app.log ]; then
     touch /app/app.log
 fi
 
-# Print Version Information
-/app/connection-cli --version
-
 # Print Initialization Message
 echo "Connection CLI is running in daemon mode." >> /app/app.log
 echo "To run tests, set the MODE environment variable to one of: mysql, postgres, redis, port, http" >> /app/app.log
 echo "Example: docker run -e MODE=port -e HOST=example.com -e PORT=80 connection-cli" >> /app/app.log
+
+# Print Version Information
+/app/connection-cli --version  >> /dev/null
 
 # Output the log file to the console
 tail -f /app/app.log
